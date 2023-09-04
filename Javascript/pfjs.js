78591afaf6cab532da5fa5d6af2cecf628a39d1d
Aryz0n <Aryz0n@outlook.com>
2023-09-04 07:13:59 +0200

    Added CV button and changed most of the text

--- Javascript/pfjs.js
+++ Javascript/pfjs.js
@@ -12,18 +12,28 @@ function divswitch(a){
             document.getElementById("div22").style.display = "block";
             document.getElementById("div23").style.display = "none";
             document.getElementById("div24").style.display = "none";
+            document.getElementById("div25").style.display = "none";
             break;
             case 3:
             document.getElementById("div21").style.display = "none";
             document.getElementById("div22").style.display = "none";
             document.getElementById("div23").style.display = "block";
             document.getElementById("div24").style.display = "none";
+            document.getElementById("div25").style.display = "none";
             break;
             case 4:
             document.getElementById("div21").style.display = "none";
             document.getElementById("div22").style.display = "none";
             document.getElementById("div23").style.display = "none";
             document.getElementById("div24").style.display = "block";
+            document.getElementById("div25").style.display = "none";
+            break;
+            case 5:
+            if (document.getElementById("div25").style.display == "block"){
+                document.getElementById("div25").style.display = "none";
+            } else {
+            document.getElementById("div25").style.display = "block";
+            }
             break;
         }
 }
